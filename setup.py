06d1e2a4e494c850458877501384c6b468701dbe
Terry Stewart <tcstewar@uwaterloo.ca>
2016-10-12 19:16:40 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,25 @@ from setuptools import setup
 setup(
     name='pytry',
     packages=['pytry'],
+    version='0.9.0',
     entry_points=dict(
         console_scripts=['pytry = pytry.cmdline:run'],
         ),
     author='Terry Stewart',
+    description='Running trials with parameter variation',
+    long_description='''
+Pytry is a light-weight Python package for defining something that you want
+to try, some parameters you'd like to vary while you try it, and some
+measurement that you want to do each time you try it.''',
     author_email='terry.stewart@gmail.com',
     url='https://github.com/tcstewar/pytry',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        ]
     )
