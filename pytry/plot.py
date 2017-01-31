443e257ea458150e1a6d59bc1e405bef77f50935
Terry Stewart <tcstewar@uwaterloo.ca>
2017-01-31 00:30:44 -0500

    Fixed silly typo

--- pytry/plot.py
+++ pytry/plot.py
@@ -22,14 +22,15 @@ class PlotTrial(trial.Trial):
 
         title = p.data_filename
 
-        if self.plt is not None and p.plt_overlay:
-            param_text = self.generate_param_text(p)
-            if result is not None:
-                result_text = self.generate_result_text(result)
-                title = '%s\n%s' % (title, result_text)
+        if self.plt is not None:
+            if p.plt_overlay:
+                param_text = self.generate_param_text(p)
+                if result is not None:
+                    result_text = self.generate_result_text(result)
+                    title = '%s\n%s' % (title, result_text)
 
-            self.plt.figtext(0.13, 0.12, param_text)
-        self.plt.suptitle(title, fontsize=8)
+                self.plt.figtext(0.13, 0.12, param_text)
+            self.plt.suptitle(title, fontsize=8)
 
         if p.plt_save:
             fn = os.path.join(p.data_dir, p.data_filename)
