7bdf3884b5789f1814c1a4084a9d9b7f0425e6f9
Terry Stewart <tcstewar@uwaterloo.ca>
2017-03-03 05:34:31 -0500

    Handle zip files

--- pytry/read.py
+++ pytry/read.py
@@ -1,24 +1,39 @@
 import os
-
+import zipfile
 
 def read(path):
     data = []
     if os.path.exists(path):
         for fn in os.listdir(path):
             fn = os.path.join(path, fn)
-            try:
-                if fn.endswith('.txt'):
-                    data.append(text(fn))
-                elif fn.endswith('.npz'):
-                    data.append(npz(fn))
-            except:
-                print('Error reading file "%s"' % fn)
+            if fn.endswith('.zip'):
+                z = zipfile.ZipFile(fn)
+                for name in z.namelist():
+                    try:
+                        if name.endswith('.txt'):
+                            data.append(text(z.open(name)))
+                        elif name.endswith('.npz'):
+                            data.append(npz(z.open(name)))
+                    except:
+                        print('Error reading file "%s" in "%s"' % (name, fn))
+                z.close()
+            else:
+                try:
+                    if fn.endswith('.txt'):
+                        data.append(text(fn))
+                    elif fn.endswith('.npz'):
+                        data.append(npz(fn))
+                except:
+                    print('Error reading file "%s"' % fn)
     return data
 
 
 def text(fn):
-    with open(fn) as f:
-        text = f.read()
+    if not hasattr(fn, 'read'):
+        with open(fn) as f:
+            text = f.read()
+    else:
+        text = fn.read()
     d = {}
     try:
         import numpy
